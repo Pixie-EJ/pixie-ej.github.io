f77e770d90a511ecc8972c78aea29e2038d21970
Julio Buda <juliopaesmoreira@hotmail.com>
2020-12-17 20:13:55 -0300

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
     */
-    (function($) {
+   (function ($) {
     "use strict"; // Start of use strict
   
     // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -21,7 +21,7 @@
     });
   
     // Scroll to top button appear
-    $(document).scroll(function() {
+    $(document).scroll(function () {
       var scrollDistance = $(this).scrollTop();
       if (scrollDistance > 100) {
         $('.scroll-to-top').fadeIn();
@@ -31,7 +31,7 @@
     });
   
     // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
+    $('.js-scroll-trigger').click(function () {
       $('.navbar-collapse').collapse('hide');
     });
   
@@ -42,7 +42,7 @@
     });
   
     // Collapse Navbar
-    var navbarCollapse = function() {
+    var navbarCollapse = function () {
       if ($("#mainNav").offset().top > 100) {
         $("#mainNav").addClass("navbar-shrink");
       } else {
@@ -55,15 +55,67 @@
     $(window).scroll(navbarCollapse);
   
     // Floating label headings for the contact form
-    $(function() {
-      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+    $(function () {
+      $("body").on("input propertychange", ".floating-label-form-group", function (e) {
         $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-      }).on("focus", ".floating-label-form-group", function() {
+      }).on("focus", ".floating-label-form-group", function () {
         $(this).addClass("floating-label-form-group-with-focus");
-      }).on("blur", ".floating-label-form-group", function() {
+      }).on("blur", ".floating-label-form-group", function () {
         $(this).removeClass("floating-label-form-group-with-focus");
       });
     });
   
-  })(jQuery); // End of use strict
+    //Add New Duende
+    var form = document.querySelector("#contactForm");
+    $('#sendMessageButton').click(function (event) {
+      event.preventDefault();
+      
+      var duende = {
+        name: form.name.value,
+        email: form.email.value,
+        phone: form.phone.value,
+        message: form.message.value
+      };
   
+      let duendeTr = document.createElement("tr");
+      let table = document.querySelector('#table-person');
+  
+      duendeTr.appendChild(createTh(duende.name));
+      duendeTr.appendChild(createTh(duende.email));
+      duendeTr.appendChild(createTh(duende.phone));
+      duendeTr.appendChild(createTh(duende.message));
+  
+      table.appendChild(duendeTr); 
+    })
+    
+    function createTh(data) {
+      var td = document.createElement("th");
+    
+      td.textContent = data;
+    
+      return td;
+    }
+  
+    // Inserir tabela duendes
+    $(document).ready(() => {
+      $('#table-person').show();
+  
+      var nomes = ["Eduardo Saito", "Luan Halaiko", "Thiago Teixeira", "João Victor"];
+      var email = ["eduardo@gmail.com", "luan@gmail.com", "thiago@gmail.com", "jv@gmail.com"];
+      var contatos = ["(42)98432-6218", "(42)96473-2738", "(42)92738-2873", "(42)99742-2833"];
+      var points = ["1200", "1700", "1550", "1670"];
+  
+      var table = '';
+      var pessoas = nomes.length;
+      var x = 0;
+      while (x < pessoas) {
+        table += '<tr><th>' + nomes[x] + '</th>';
+        table += '<th>' + email[x] + '</th>';
+        table += '<th>' + contatos[x] + '</th>';
+        table += '<th>' + points[x] + '</th></tr>';
+        x++;
+      }
+      $('#table-person tbody').html(table);
+    })
+  
+  })(jQuery); // End of use strict
